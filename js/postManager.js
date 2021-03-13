6e30c7badc9ed35038893498cdf7bae36d5f37f9
Paola Aguilar <pao.aquev29@gmail.com>
2021-03-13 17:16:33 -0600

    adding dropdown to the form

--- js/postManager.js
+++ js/postManager.js
@@ -1,4 +1,4 @@
-// import JsonRequestSingleton from "./singleton.js";
+import JsonRequestSingleton from "./singleton.js";
 
 const searchMenuButton = document.querySelector("#search");
 const homeMenuButton = document.querySelector("#home");
@@ -11,3 +11,35 @@ homeMenuButton.addEventListener("click", (e) => {
   e.preventDefault();
   window.location.href = "../index.html";
 });
+
+const showAuthors = async () => {
+  try {
+    const authors = await JsonRequestSingleton.getInstance().getRequest(
+      `authors`
+    );
+    console.log(authors);
+    const authorInput = document.querySelector("#author");
+    const dropDown = document.querySelector(".dropdown");
+
+    authorInput.value = `${authors[0].name} ${authors[0].lastName}`;
+
+    authorInput.addEventListener("click", () => {
+      console.log("hi");
+      authors.map((result) => {
+        const authorElement = document.createElement("li");
+        const line = document.createElement("hr");
+        console.log(result);
+        authorElement.classList.add("author-dropdown");
+        authorElement.innerText = `${result.name} ${result.lastName}`;
+        authorElement.addEventListener("click", () => {
+          authorInput.value = `${result.name} ${result.lastName}`;
+          dropDown.innerHTML = "";
+        });
+        dropDown.appendChild(authorElement);
+        dropDown.appendChild(line);
+      });
+    });
+  } catch (error) {}
+};
+
+showAuthors();
---
+++ js/requiredElements.js
@@ -0,0 +1,3 @@
+// const movieName = document.querySelector("#movie");
+// movieName.setAttribute("required", ""); //turns required on
+// movieName.required = true;
